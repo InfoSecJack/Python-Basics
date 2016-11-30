7d524e70b865081c91d018d7052bf21c718b42c8
Synthestra <jace.b@ntlworld.com>
2016-11-30 23:56:54 +0000

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -2,13 +2,25 @@
 import sys, time, random
 class lesson(object):
 
-
     def __init__(self, lessonType):
         self.lesson = lesson
         getattr(lesson,lessonType)()
     def lesson1():
-        printfunc = "Hello! Welcome to the Variables Tutorial!\n"
-        for x in printfunc:
-            sys.stdout.write(x)
-            sys.stdout.flush()
-            time.sleep(random.uniform(0,0.05))
+        def rlsmTyp(sentence):
+            for l in sentence:
+                sys.stdout.write(l)
+                sys.stdout.flush()
+                time.sleep(random.uniform(0,0.1))
+
+        rlsmTyp(
+"""Lesson: Variables
+
+Todo:
+Acceptable variables
+
+Variable names cannot BEGIN with a number
+Variable1 = Sure, but poorly named
+myVar = Yes
+my_var = Yes
+
+""")
