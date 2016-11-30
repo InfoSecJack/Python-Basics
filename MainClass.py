eefcaf99e11c741eb0292dc4d7181a7856bba942
Synthestra <jace.b@ntlworld.com>
2016-11-30 22:32:52 +0000

    Update MainClass.py

--- MainClass.py
+++ MainClass.py
@@ -4,14 +4,22 @@ topics = [
     "Print",
     "Variables",
     "Inputs",
-    "If Elif Else",
-    "Loops"]
+    "Loops",
+    "test"]
+lentopic = len(topics)
+hide = 37 - lentopic
 
-
-for x in range(len(topics)):
-    print (str(x+1) + ") " + topics[x])
-userinput = int(input("Pick a topic number: "))
-userinput = topics[userinput -1]
-i=importlib.import_module(userinput)
+while True:
+    for x in range(lentopic):
+        print(str(x+1) + ") " + topics[x])
+    try:
+        userinput = int(input("Pick a topic number: "))
+        if userinput in range(1,lentopic):
+            break
+    except:
+        print("Please input a number")
+        print("\n"*hide)
+print("\n"*39)
+userinput = topics[userinput-1]
+i = importlib.import_module(userinput)
 i.lesson("lesson1")
-input("")
