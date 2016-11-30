bfcb9a21fada85072baa6b57066da479104a0a46
Synthestra <jace.b@ntlworld.com>
2016-11-30 22:33:48 +0000

    Update Print.py

--- Print.py
+++ Print.py
@@ -7,8 +7,39 @@ class lesson(object):
         self.lesson = lesson
         getattr(lesson,lessonType)()
     def lesson1():
-        printfunc = "Hello! Welcome to the Print Tutorial!\n"
-        for x in printfunc:
-            sys.stdout.write(x)
-            sys.stdout.flush()
-            time.sleep(random.uniform(0,0.05))
+        def rlsmTyp(sentence):
+            for l in sentence:
+                sys.stdout.write(l)
+                sys.stdout.flush()
+                time.sleep(random.uniform(0,0.1))
+
+        rlsmTyp(
+"""Lesson: Printing
+
+Hello - This is the tutorial on how to print something to the screen!
+
+print("Use this as an example for your print function!")
+""")
+        print(
+"""=========================
+Use this as an example for your print function!
+============================""")
+
+        rlsmTyp(
+"""
+As you can see, there is a name that calls the function, the function has
+an opening and closing brackets, and quotations inside them. Remember this!
+
+Lets start you off the traditional way - \"Hello World!\"
+
+OBJECTIVE == Print \"Hello World!\" to the screen
+""")
+        answer = "print(\"Hello World!\")"
+        tutIn = input(
+"""=========================\n#Now it's your turn - Enter your code below
+""")
+        while True:
+            if tutIn == answer:
+                break
+            else:
+                print("ERROR: yourCode !=",answer)
