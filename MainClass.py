66adf8ec3e075434e42a7ced1189bfb1eda36f66
Synthestra <jace.b@ntlworld.com>
2016-12-10 23:50:17 +0000

    Fixed KeyboardInterrupt in the Menu

--- MainClass.py
+++ MainClass.py
@@ -19,20 +19,22 @@ def main():
         try:
             userinput = int(input("Pick a topic number: "))
             if userinput in range(1,len(topics)+1):
-                print("\n"*39)
+                print("\n"*50)
                 userinput = topics[userinput-1]
                 try:
                     i = importlib.import_module(userinput)
                     i.lesson("lesson1")
                 except KeyboardInterrupt:
-                    print("\n"*39)
-                    print("You have quit the current lesson")
-                    print("\n")
+                    print("\n"*50)
+                    print("You have quit the current lesson\n")
             else:
                 print("Please input a number within 1 and {}".format(len(topics)))
         except ValueError:
-            print("That wasn't a number!")
-            print("\n"*39)
+            print("\n"*50)
+            print("That wasn't a number!\n")
+        except KeyboardInterrupt:
+            print("\n"*50)
+                
             
 if __name__ == "__main__":
     main()
